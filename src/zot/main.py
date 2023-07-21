360f1cdef8c1ba841a680aa3aa26461ce049c53f
Lars van Vianen <lars@vianen.com>
2023-07-21 23:26:55 +0200

    Create main.py

---
+++ src/zot/main.py
@@ -0,0 +1,9 @@
+from pyzotero import zotero
+
+
+zot = zotero.Zotero(library_id, library_type, api_key)
+items = zot.top(limit=5)
+# we've retrieved the latest five top-level items in our library
+# we can print each item's item type and ID
+for item in items:
+    print('Item: %s | Key: %s' % (item['data']['itemType'], item['data']['key']))
